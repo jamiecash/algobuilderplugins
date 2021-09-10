3b203f6261545ad86729b821a1d452526a045388
Jamie Cash <jlcash@gmail.com>
2021-09-10 15:43:15 +0100

    Finished moving average feature calculation

--- src/feature/movingaverage/movingaverage.py
+++ src/feature/movingaverage/movingaverage.py
@@ -1,6 +1,7 @@
 import logging
 
-from features import feature as ft
+from algobuilder.utils import DatabaseUtility
+from feature import feature as ft, models
 
 
 class MovingAverage(ft.FeatureImplementation):
@@ -18,7 +19,33 @@ class MovingAverage(ft.FeatureImplementation):
         :return:
         """
         # Get the symbols. There should only be 1
-        assert len(feature_execution.featureexecutionsymbol_set) == 1
-        symbol = feature_execution.featureexecutionsymbol_set[0].datasource_symbol
+        assert len(feature_execution.featureexecutiondatasourcesymbol_set.all()) == 1
+        feds = feature_execution.featureexecutiondatasourcesymbol_set.all()[0]
 
-        self.__log.debug(f"Calculating moving average for {feature_execution}. Symbol={symbol}")
+        self.__log.info(f"Calculating moving average for {feature_execution}. FeatureExecutionDataSourceSymbol={feds}")
+
+        # Get the data. Candles that don't have a feature calculated and those that do but are required for teh first
+        # calculation.
+        data = ft.FeatureImplementation.get_data(feds)
+
+        # If get_data returned None, then we don't have any features to calculate. Otherwise calculate the moving
+        # average from the data.
+        if data is not None:
+            # Calculate the moving average
+            data['moving_average'] = data['bid_close'].rolling(feature_execution.calculation_period).mean()
+
+            # Reshape the dataframe for upload into the feature_execution_result table. This will require candle_id,
+            # feature_execution_id and result
+            data = data.reset_index()
+            data['feature_execution_id'] = feature_execution.id
+            data['result'] = data['moving_average']
+            data = data.drop(labels=data.columns.difference(['time', 'feature_execution_id', 'result']), axis=1)
+
+            # Save the calculations
+            DatabaseUtility.bulk_insert_or_update(data=data,
+                                                  table=models.FeatureExecutionResult.objects.model._meta.db_table,
+                                                  batch_size=1000)
+
+        else:
+            self.__log.info(f"Feature calculations up to date. No new features calculated for "
+                            f"{feds.datasource_symbol.symbol.name}.")
